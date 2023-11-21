a1ed9af5ed4bf962b6c77ba0ebe874af7f8a4ee5
jeevanthask <jeevanthanandika@gmail.com>
2023-11-21 14:58:26 +0530

    upgrade

--- Validating-request-data-with-pipes/example-01/src/students/students.controller.ts
+++ Validating-request-data-with-pipes/example-01/src/students/students.controller.ts
@@ -1,9 +1,21 @@
-import { Get, Controller } from '@nestjs/common';
+import { Get, Controller, Post, Body, Param } from '@nestjs/common';
+import { CreateStudentDto } from './dtos/createStudent.dto';
 
 @Controller('students')
 export class StudentsController {
   @Get()
   getAllStudents() {
     return 'here are all the students';
+  }
+
+  @Post()
+  createStudent(@Body() body: CreateStudentDto) {
+    console.log(body);
+    return 'student created successfully';
+  }
+
+  @Get('/:id')
+  getStudent(@Param('id') id: string) {
+    return 'student id is ' + id;
   }
 }
